5115ec50d7be3d4140290d9c9cb96a96c471fa8b
Powell, Tyler <tspowell@tampabay.rr.com>
2019-08-29 16:21:30 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ISM4300MYFIRSTAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first Console App");
+            Console.WriteLine("I am learning how to publish to GitHub");
+        }
+    }
+}
